f26bf55a45d428d7ca9ca0fe22f5801b9bae071a
jominjimail <jominjimail@hanyang.ac.kr>
2019-06-28 21:09:20 +0900

    Controller Training

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,17 +1,59 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 @Controller
 @RequestMapping("/sample/*")
 public class SampleController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
 	@RequestMapping("")
 	public void basic() {
 		logger.info("basic...............");
 	}
+	
+	@RequestMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		logger.info(""+ dto);
+		return "ex01";
+	}
+	
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids")ArrayList<String> ids) {
+		logger.info("ids: "+ids);
+		return "ex02List";
+	}
+	
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		logger.info("list dtos : " + list);
+		return "ex02Bean";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		logger.info("todo : "+todo);
+		return "ex03";
+	}
+	
 }
